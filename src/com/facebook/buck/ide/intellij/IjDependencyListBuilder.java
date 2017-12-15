2a1e19da27d3d06d441e28263fd6f81fb58930c1
Tamás Bártfai <lt@digitalasset.com>
2017-12-15 13:35:44 +0100

    Use COMPILE scope for shadow dependencies
    The provided scope doesn't properly propagate the
    dependency.

--- src/com/facebook/buck/ide/intellij/IjDependencyListBuilder.java
+++ src/com/facebook/buck/ide/intellij/IjDependencyListBuilder.java
@@ -167,7 +167,7 @@ public class IjDependencyListBuilder {
             .setData(
                 DependencyEntryData.builder()
                     .setName(name)
-                    .setScope(Scope.PROVIDED)
+                    .setScope(Scope.COMPILE)
                     .setExported(true)
                     .build())
             .build());
